13e9e72af5dc671c1d4fa879cce334630b0917bd
sheehanmunim <sheehanmunim@gmail.com>
2025-07-22 18:19:48 -0400

    fix: add defensive checks and type safety to prevent EXC_BAD_ACCESS in BluetoothPeripheral

--- ios/BluetoothPeripheral.mm
+++ ios/BluetoothPeripheral.mm
@@ -17,23 +17,28 @@ RCT_EXPORT_MODULE()
     return self;
 }
 
-// Update method signature to match protocol (use id)
+// Defensive: Ensure method signature matches protocol and type check options
 - (void)startAdvertising:(id)options {
     if (self.peripheralManager.state != CBManagerStatePoweredOn) {
         // Peripheral manager not ready
+        return;
+    }
+    if (![options isKindOfClass:[NSDictionary class]]) {
         return;
     }
     NSDictionary *dictOptions = (NSDictionary *)options;
     NSString *localName = dictOptions[@"localName"];
     NSArray *serviceUUIDs = dictOptions[@"serviceUUIDs"];
     NSMutableDictionary *advertisingData = [NSMutableDictionary dictionary];
-    if (localName) {
+    if ([localName isKindOfClass:[NSString class]]) {
         advertisingData[CBAdvertisementDataLocalNameKey] = localName;
     }
-    if (serviceUUIDs) {
+    if ([serviceUUIDs isKindOfClass:[NSArray class]]) {
         NSMutableArray *uuids = [NSMutableArray array];
-        for (NSString *uuidStr in serviceUUIDs) {
-            [uuids addObject:[CBUUID UUIDWithString:uuidStr]];
+        for (id uuidStr in serviceUUIDs) {
+            if ([uuidStr isKindOfClass:[NSString class]]) {
+                [uuids addObject:[CBUUID UUIDWithString:uuidStr]];
+            }
         }
         advertisingData[CBAdvertisementDataServiceUUIDsKey] = uuids;
     }
@@ -47,21 +52,29 @@ RCT_EXPORT_METHOD(stopAdvertising) {
 RCT_EXPORT_METHOD(setServices:(NSArray *)services) {
     [self.services removeAllObjects];
     for (NSDictionary *serviceDict in services) {
+        if (![serviceDict isKindOfClass:[NSDictionary class]]) continue;
         NSString *uuid = serviceDict[@"uuid"];
+        if (![uuid isKindOfClass:[NSString class]]) continue;
         CBMutableService *service = [[CBMutableService alloc] initWithType:[CBUUID UUIDWithString:uuid] primary:YES];
         NSMutableArray *characteristics = [NSMutableArray array];
-        for (NSDictionary *charDict in serviceDict[@"characteristics"]) {
+        NSArray *charArray = serviceDict[@"characteristics"];
+        if (![charArray isKindOfClass:[NSArray class]]) charArray = @[];
+        for (NSDictionary *charDict in charArray) {
+            if (![charDict isKindOfClass:[NSDictionary class]]) continue;
             NSString *charUUID = charDict[@"uuid"];
             NSArray *propertiesArr = charDict[@"properties"];
             CBCharacteristicProperties properties = 0;
-            for (NSString *prop in propertiesArr) {
-                if ([prop isEqualToString:@"read"]) properties |= CBCharacteristicPropertyRead;
-                if ([prop isEqualToString:@"write"]) properties |= CBCharacteristicPropertyWrite;
-                if ([prop isEqualToString:@"notify"]) properties |= CBCharacteristicPropertyNotify;
-                if ([prop isEqualToString:@"indicate"]) properties |= CBCharacteristicPropertyIndicate;
+            if ([propertiesArr isKindOfClass:[NSArray class]]) {
+                for (NSString *prop in propertiesArr) {
+                    if (![prop isKindOfClass:[NSString class]]) continue;
+                    if ([prop isEqualToString:@"read"]) properties |= CBCharacteristicPropertyRead;
+                    if ([prop isEqualToString:@"write"]) properties |= CBCharacteristicPropertyWrite;
+                    if ([prop isEqualToString:@"notify"]) properties |= CBCharacteristicPropertyNotify;
+                    if ([prop isEqualToString:@"indicate"]) properties |= CBCharacteristicPropertyIndicate;
+                }
             }
             NSData *value = nil;
-            if (charDict[@"value"]) {
+            if ([charDict[@"value"] isKindOfClass:[NSString class]]) {
                 value = [charDict[@"value"] dataUsingEncoding:NSUTF8StringEncoding];
                 // If value is present, characteristic must be read-only
                 properties = CBCharacteristicPropertyRead;
@@ -70,8 +83,10 @@ RCT_EXPORT_METHOD(setServices:(NSArray *)services) {
             if (value) {
                 permissions = CBAttributePermissionsReadable;
             }
-            CBMutableCharacteristic *characteristic = [[CBMutableCharacteristic alloc] initWithType:[CBUUID UUIDWithString:charUUID] properties:properties value:value permissions:permissions];
-            [characteristics addObject:characteristic];
+            if ([charUUID isKindOfClass:[NSString class]]) {
+                CBMutableCharacteristic *characteristic = [[CBMutableCharacteristic alloc] initWithType:[CBUUID UUIDWithString:charUUID] properties:properties value:value permissions:permissions];
+                [characteristics addObject:characteristic];
+            }
         }
         service.characteristics = characteristics;
         [self.services addObject:service];
@@ -84,15 +99,17 @@ RCT_EXPORT_METHOD(setServices:(NSArray *)services) {
 
 - (NSNumber *)multiply:(double)a b:(double)b {
     NSNumber *result = @(a * b);
-
     return result;
 }
 
+// Defensive: Only compile this if C++/TurboModule is available
+#ifdef __cplusplus
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
     (const facebook::react::ObjCTurboModule::InitParams &)params
 {
     return std::make_shared<facebook::react::NativeBluetoothPeripheralSpecJSI>(params);
 }
+#endif
 
 // Add stubs for required CBPeripheralManagerDelegate methods
 - (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral {
